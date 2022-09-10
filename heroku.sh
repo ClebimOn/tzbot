b30df753bc4d02455b451d32298ea1bd858df540
blabla <blablablabla@gmail.com>
2022-09-10 11:51:56 -0300

    blabla

---
+++ heroku.sh
@@ -0,0 +1,5 @@
+git config --global user.email "blablablabla@gmail.com" && git config --global user.name "blabla" && git add . && git commit -m "heroku" && git push heroku master
+echo ${RED}'Upload Concluído
+
+Pressione enter para Finalizar'
+read next
